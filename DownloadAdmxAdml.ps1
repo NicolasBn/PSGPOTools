c3934197ef190b479af50692e9bb3c2bc7b6e62c
NicolasBN <b.nicolas.49@gmail.com>
2020-10-05 17:28:43 +0200

    Change (architecture) : Sampler migration Second step. remove unecessary folder and task

--- DownloadAdmxAdml.ps1
+++ DownloadAdmxAdml.ps1
@@ -1,6 +1,6 @@
 
 $AdmxURI = 'https://download.microsoft.com/download/9/2/0/9200837E-1415-40C0-BE02-BDE308606DC5/Administrative%20Templates%20(.admx)%20for%20Windows%2010%20May%202019%20Update%20v3.msi'
-$Target = 'output\AdmxW10_1903.msi'
+$Target = 'output\RequiredModules\AdmxW10_1903.msi'
 
 (new-object System.Net.WebClient).DownloadFile($AdmxUri,$Target)
 
